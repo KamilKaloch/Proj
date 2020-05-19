6c142e525ed9823a74096b3c26f1caa054f55b12
KamilKaloch <51744352+KamilKaloch@users.noreply.github.com>
2020-05-19 16:49:26 +0200

    Upload

---
+++ MiniTC/MiniTC/PanelTC.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MiniTC
+{
+    public partial class PanelTC : UserControl
+    {
+        public PanelTC()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MiniTC/MiniTC/ViewModels/MainVM.cs
@@ -0,0 +1,38 @@
+﻿namespace MiniTC.ViewModels
+{
+    using BaseClass;
+    using System.IO;
+    using System;
+    internal partial class MainVM : ViewModelBase
+    {
+        static string[] drives = Directory.GetLogicalDrives();
+        static string currentDrive = drives[0];
+        static private string currentPath = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System));
+        static private string[] availablePaths = GetAvailablePaths();
+        public string[] Drives { get => drives; }
+        public string CurrentDrive { get => currentDrive; set => currentDrive = value; }
+        public string CurrentPath { get => currentPath; set => currentPath = value; }
+        public string[] AvailablePaths { get => availablePaths; set => availablePaths = value; }
+
+        static private string[] GetAvailablePaths()
+        {
+            string[] temp1 = Directory.GetDirectories(currentPath);
+            for (int i = 0; i < temp1.Length; i++)
+            {
+                temp1[i] = Path.GetFileName(temp1[i]);
+                temp1[i] = $"<D>{temp1[i]}";
+            }
+
+            string[] temp2 = Directory.GetFiles(currentPath);
+            for (int i = 0; i < temp2.Length; i++)
+                temp2[i] = Path.GetFileName(temp2[i]);
+
+            string[] temp3 = new string[temp1.Length + temp2.Length];
+
+            Array.Copy(temp1, temp3, temp1.Length);
+            Array.Copy(temp2, 0, temp3, temp1.Length, temp2.Length);
+
+            return temp3;
+        }
+    }
+}
